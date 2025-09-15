0beabc61d888c27502ad1b6597c25722a06b2468
KhangLmao450 <nguyentramy12092005@gmail.com>
2025-09-15 11:37:47 +0700

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,67 @@
+import requests,json,time,os,threading 
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+PORT = int(os.environ.get("PORT", 5000))
+
+login_data = {
+
+    "username": "BoxMineSs6", # đổi thành username acc mcserver vào đây
+
+    "email": "", # hoặc dùng mail để mail thì thôi username 
+
+    "password": {
+
+        "value": "012686az@", # password điền vào 
+
+        "repeat": ""
+
+    }
+
+}
+
+sub_url = "https://www.mcserverhost.com/api/servers/8957c950/subscription" # dán vào đây 
+
+def run_automation():
+
+    session = requests.Session()
+
+    while True:
+
+        response = session.post("https://www.mcserverhost.com/api/login", headers={'Content-Type': 'application/json'}, data=json.dumps(login_data))
+
+        if response.status_code == 200:
+
+            session.post(sub_url, headers={'Content-Type': 'application/json'}, data=json.dumps({}))
+
+        elif response.status_code == 406:
+
+            print("Sai tài khoản hoặc mật khẩu rồi!")
+
+        elif response.status_code == 403:
+
+            print("IP của bạn có thể đã bị chặn bởi website")
+
+        else:
+
+            print(f"Lỗi {response.status_code} - {response.text}")
+
+        time.sleep(3000)
+
+@app.route('/')
+
+def home():
+
+    return "Renew đang chạy."
+
+if __name__ == '__main__':
+
+    au= threading.Thread(target=run_automation)
+
+    au.daemon = True
+
+    au.start()
+
+    app.run(host='0.0.0.0', port=PORT)
